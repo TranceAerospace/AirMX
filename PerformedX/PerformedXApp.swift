db146b8191c742287a114104a26b93f077dd5eef
Trance <xp46290@gmail.com>
2023-02-24 17:54:12 -0500

    Initial Commit

---
+++ PerformedX/PerformedXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerformedXApp.swift
+//  PerformedX
+//
+//  Created by Mark Trance on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PerformedXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
