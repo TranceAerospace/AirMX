0b36f81a3f54105cb99ed84bbdf70ec10bd79375
Trance <xp46290@gmail.com>
2023-03-03 15:58:57 -0500

    Create a screen for adding parts to a work order

--- AirMX/Views/AddPartsView.swift
+++ AirMX/Views/AddPartsView.swift
@@ -9,21 +9,65 @@ import SwiftUI
 
 struct AddPartsView: View {
     @Environment(\.dismiss) var dismiss
+    @Environment(\.managedObjectContext) var moc
+    
+    @State private var partNumberOff = ""
+    @State private var serialNumberOff = ""
+    @State private var partNumberOn = ""
+    @State private var serialNumberOn = ""
+    @Binding var newPart: Part?
     
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            PartsRowView(label: "Part Number Off", passedText: $partNumberOff)
+            PartsRowView(label: "Serial Number Off", passedText: $serialNumberOff)
+            PartsRowView(label: "Part Number On", passedText: $partNumberOn)
+            PartsRowView(label: "Serial Number On", passedText: $serialNumberOn)
+            
             Button {
+                newPart = Part(context: moc)
+                if let newPart {
+
+                    newPart.partNumberOff = partNumberOff.isEmpty ? "No old p/n" : partNumberOff
+                    newPart.serialNumberOff = serialNumberOff.isEmpty ? "No old s/n": serialNumberOff
+                    newPart.partNumberOn = partNumberOn.isEmpty ? "no new p/n" : partNumberOn
+                    newPart.serialNumberOn = serialNumberOn.isEmpty ? "no new s/n": serialNumberOn
+                    
+                    
+                    print(newPart.partNumberOff)
+                }
                 dismiss()
+        
             } label: {
-                Text("Dismiss")
+                Text("Add Part")
             }
         }
+        .presentationDetents([.medium])
     }
 }
 
-struct AddPartsView_Previews: PreviewProvider {
-    static var previews: some View {
-        AddPartsView()
+//struct AddPartsView_Previews: PreviewProvider {
+//    let newPart = Part.empty
+//    static var previews: some View {
+//        let newParts = [Part.empty]
+//        AddPartsView()
+//    }
+//}
+
+struct PartsRowView: View {
+    var label: String
+    @Binding var passedText: String
+    
+    var body: some View {
+        HStack {
+            Text(label)
+            TextField("", text: $passedText)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(.black, lineWidth: 1))
+                //.border(.black)
+            
+        }
+        .padding()
     }
 }
